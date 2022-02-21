65097ba8a2c3cafc88efb3a9f7f342400bd1672f
Van Darkholme <37331560+0Trickster0@users.noreply.github.com>
2022-02-21 13:54:58 -0500

    Add Project 03 file

---
+++ Project03_Team4.py
@@ -0,0 +1,181 @@
+from typing import List, Dict
+import time
+import prettytable
+
+
+month_abbrev = {'JAN': 1, 'FEB': 2, 'MAR': 3, 'APR': 4, 'MAY': 5, 'JUN': 6,
+                'JUL': 7, 'AUG': 8, 'SEP': 9, 'OCT': 10, 'NOV': 11, 'DEC': 12}
+
+
+class GEDReader:
+    def __init__(self):
+        self.individual_dic: Dict[str, Individual] = {}
+        self.family_dic: Dict[str, Family] = {}
+        self.__cur_individual: Individual or None = None
+        self.__cur_family: Family or None = None
+
+    def read_ged_data(self, file_path):
+        # Function that reads the content of the file
+        try:
+            f = open(file_path)
+        except FileNotFoundError:
+            print('FIle path is invalid!')
+            return
+
+        lines: List[str] = f.readlines()
+        self.process_line(lines)
+
+    def process_line(self, lines: List[str]):
+        # Function that processes each line of the file according to its key word
+        for i, line in enumerate(lines):
+            words = line.split()
+            if len(words) >= 3 and words[2] == 'INDI':
+                self.add_data()
+                self.__cur_individual = Individual(words[1].strip('@'))
+            elif len(words) >= 3 and words[2] == 'FAM':
+                self.add_data()
+                self.__cur_family = Family(words[1].strip('@'))
+            elif len(words) >= 2 and words[1] == 'DATE':
+                year = int(words[-1])
+                month = words[-2]
+                if month not in month_abbrev.keys():
+                    month = None
+                else:
+                    month = month_abbrev[month]
+                day = words[-3]
+                if not month:
+                    day = None
+                else:
+                    try:
+                        day = int(day)
+                    except ValueError:
+                        day = None
+                date: Date = Date(year, month, day)
+                last_line = lines[i-1].split()
+                if last_line[1] == 'BIRT' and self.__cur_individual:
+                    self.__cur_individual.birthday = date
+                elif last_line[1] == 'DEAT' and self.__cur_individual:
+                    self.__cur_individual.death_date = date
+                elif last_line[1] == 'MARR' and self.__cur_family:
+                    self.__cur_family.married_date = date
+                elif last_line[1] == 'DIV' and self.__cur_family:
+                    self.__cur_family.divorced_date = date
+            elif len(words) >= 3 and words[1] == 'NAME' and self.__cur_individual:
+                self.__cur_individual.name = ' '.join(words[2:])
+            elif len(words) >= 3 and words[1] == 'SEX' and self.__cur_individual:
+                self.__cur_individual.gender = 'Male' if words[2] == 'M' else 'Female'
+            elif len(words) >= 3 and words[1] == 'HUSB' and self.__cur_family:
+                self.__cur_family.husband = self.individual_dic[words[2].strip('@')]
+            elif len(words) >= 3 and words[1] == 'WIFE' and self.__cur_family:
+                self.__cur_family.wife = self.individual_dic[words[2].strip('@')]
+            elif len(words) >= 3 and words[1] == 'CHIL' and self.__cur_family:
+                self.__cur_family.child.append(self.individual_dic[words[2].strip('@')])
+        self.post_process()
+
+    def post_process(self):
+        # Post process the child and spouse variable of each individual
+        for family in self.family_dic.values():
+            if not family.is_divorced():
+                family.husband.spouse = family.wife
+                family.wife.spouse = family.husband
+            for c in family.child:
+                family.husband.child.append(c)
+                family.wife.child.append(c)
+
+    def add_data(self):
+        # When a new individual or family object is created, add the current data to the field dictionary
+        if self.__cur_individual:
+            self.individual_dic[self.__cur_individual.id] = self.__cur_individual
+        if self.__cur_family:
+            self.family_dic[self.__cur_family.id] = self.__cur_family
+        self.__cur_family, self.__cur_individual = None, None
+
+    def print_info(self):
+        # print info with PrettyTable
+        individual_pt: prettytable = prettytable.PrettyTable()
+        individual_pt.field_names = ['ID', 'Name', 'Gender', 'Birthday', 'Age', 'Alive', 'Death', 'Child', 'Spouse']
+        for k, v in self.individual_dic.items():
+            individual_pt.add_row([k, v.name, v.gender, str(v.birthday), v.get_age(),
+                                   v.is_alive(), v.death_date, ','.join([c.id for c in v.child]),
+                                   v.spouse.id if v.spouse else None])
+        print(individual_pt)
+        family_pt: prettytable = prettytable.PrettyTable()
+        family_pt.field_names = ['ID', 'Married', 'Divorced', 'Husband ID', 'Husband Name',
+                                 'Wife ID', 'Wife Name', 'Children']
+        for k, v in self.family_dic.items():
+            family_pt.add_row([k, v.married_date, v.is_divorced(), v.husband.id, v.husband.name, v.wife.id, v.wife.name,
+                               ','.join([c.id for c in v.child])])
+        print(family_pt)
+
+
+class Individual:
+    def __init__(self, individual_id):
+        self.id: str = individual_id
+        self.name: str = ''
+        self.gender: str = ''
+        self.birthday: Date or None = None
+        self.death_date: Date or None = None
+        self.child: List[Individual] = []
+        self.spouse: Individual or None = None
+
+    def get_age(self):
+        bir = self.birthday
+        if self.is_alive():
+            t = time.localtime()
+            age = t.tm_year - bir.year
+            if t.tm_mon < bir.month or (t.tm_mon == bir.month and t.tm_mday < bir.day):
+                age -= 1
+            return age
+        else:
+            death = self.death_date
+            age = death.year - bir.year
+            if death.month < bir.month or (death.month == bir.month and death.day < bir.day):
+                age -= 1
+            return age
+
+    def is_alive(self):
+        return self.death_date is None
+
+
+class Family:
+    def __init__(self, family_id):
+        self.id: str = family_id
+        self.married_date: Date or None = None
+        self.divorced_date: Date or None = None
+        self.husband: Individual or None = None
+        self.wife: Individual or None = None
+        self.child: List[Individual] = []
+
+    def is_divorced(self):
+        return self.divorced_date is not None
+
+
+class Date:
+    def __init__(self, year, month, day):
+        self.year = year
+        self.month = month
+        self.day = day
+
+    def __str__(self):
+        if self.month is None and self.day is None:
+            return str(self.year)
+        elif self.day is None:
+            return str(self.year) + '-' + str(self.month)
+        else:
+            return str(self.year) + '-' + str(self.month) + '-' + str(self.day)
+
+    def __cmp__(self, other):
+        if self.year != other.year:
+            return -1 if self.year < other.year else 1
+        elif self.month != other.month:
+            return -1 if self.month < other.month else 1
+        elif self.day != other.day:
+            return -1 if self.day < other.day else 1
+        return 0
+
+
+if __name__ == '__main__':
+    reader = GEDReader()
+    reader.read_ged_data('project01_ShengpingXu.ged')
+    reader.print_info()
+
